2aae2fc50fa636d83e3fd42df9d6e3374ba4b996
ali hene <alihene222@gmail.com>
2024-10-26 14:45:37 +0100

    cube

--- src/main.c
+++ src/main.c
@@ -9,8 +9,8 @@
 #include <cglm/struct.h>
 #include <cglm/struct/cam.h>
 
-#define SCREEN_WIDTH 427
-#define SCREEN_HEIGHT 240
+#define SCREEN_WIDTH 854
+#define SCREEN_HEIGHT 480
 
 #define SET_PIXEL(x, y, color) if(x >= 0 && y >= 0 && x < SCREEN_WIDTH && y < SCREEN_HEIGHT) state.pixels[y * SCREEN_WIDTH + x] = color;
 
@@ -504,7 +504,7 @@ int main() {
         proj = glms_perspective(glm_rad(90.0f), (f32) SCREEN_WIDTH / (f32) SCREEN_HEIGHT, 0.1f, 100.0f);
         model = glms_rotate(model, glm_rad((f32) counter), (vec3s) {1.0f, 1.0f, 0.0f});
 
-        Vertex vertices[18];
+        Vertex vertices[36];
         vertices[0] = (Vertex) {
             (vec3s) {-1.0f, 1.0f, 1.0f},
             1.0f,
@@ -538,22 +538,22 @@ int main() {
         vertices[6] = (Vertex) {
             (vec3s) {-1.0f, -1.0f, 1.0f},
             1.0f,
-            (vec2s) {1.0f, 0.0f}
+            (vec2s) {1.0f / 8.0f, 0.0f}
         };
         vertices[7] = (Vertex) {
             (vec3s) {-1.0f, 1.0f, 1.0f},
             1.0f,
-            (vec2s) {1.0f, 1.0f}
+            (vec2s) {1.0f / 8.0f, 1.0f / 8.0f}
         };
         vertices[8] = (Vertex) {
             (vec3s) {-1.0f, 1.0f, -1.0f},
             1.0f,
-            (vec2s) {0.0f, 1.0f}
+            (vec2s) {0.0f, 1.0f / 8.0f}
         };
         vertices[9] = (Vertex) {
             (vec3s) {-1.0f, 1.0f, -1.0f},
             1.0f,
-            (vec2s) {0.0f, 1.0f}
+            (vec2s) {0.0f, 1.0f / 8.0f}
         };
         vertices[10] = (Vertex) {
             (vec3s) {-1.0f, -1.0f, -1.0f},
@@ -563,10 +563,130 @@ int main() {
         vertices[11] = (Vertex) {
             (vec3s) {-1.0f, -1.0f, 1.0f},
             1.0f,
-            (vec2s) {1.0f, 0.0f}
+            (vec2s) {1.0f / 8.0f, 0.0f}
+        };
+        vertices[12] = (Vertex) {
+            (vec3s) {-1.0f, 1.0f, 1.0f},
+            1.0f,
+            (vec2s) {0.0f, 0.0f}
+        };
+        vertices[13] = (Vertex) {
+            (vec3s) {1.0f, 1.0f, 1.0f},
+            1.0f,
+            (vec2s) {1.0f / 8.0f, 0.0f}
+        };
+        vertices[14] = (Vertex) {
+            (vec3s) {1.0f, 1.0f, -1.0f},
+            1.0f,
+            (vec2s) {1.0f / 8.0f, 1.0f / 8.0f}
+        };
+        vertices[15] = (Vertex) {
+            (vec3s) {1.0f, 1.0f, -1.0f},
+            1.0f,
+            (vec2s) {1.0f / 8.0f, 1.0f / 8.0f}
+        };
+        vertices[16] = (Vertex) {
+            (vec3s) {-1.0f, 1.0f, -1.0f},
+            1.0f,
+            (vec2s) {0.0f, 1.0f / 8.0f}
+        };
+        vertices[17] = (Vertex) {
+            (vec3s) {-1.0f, 1.0f, 1.0f},
+            1.0f,
+            (vec2s) {0.0f, 0.0f}
+        };
+        vertices[18] = (Vertex) {
+            (vec3s) {-1.0f, 1.0f, -1.0f},
+            1.0f,
+            (vec2s) {0.0f, 1.0f / 8.0f}
+        };
+        vertices[19] = (Vertex) {
+            (vec3s) {1.0f, 1.0f, -1.0f},
+            1.0f,
+            (vec2s) {1.0f / 8.0f, 1.0f / 8.0f}
+        };
+        vertices[20] = (Vertex) {
+            (vec3s){1.0f, -1.0f, -1.0f},
+            1.0f,
+            (vec2s) {1.0f / 8.0f, 0.0f}
+        };
+        vertices[21] = (Vertex) {
+            (vec3s){1.0f, -1.0f, -1.0f},
+            1.0f,
+            (vec2s) {1.0f / 8.0f, 0.0f}
+        };
+        vertices[22] = (Vertex) {
+            (vec3s){-1.0f, -1.0f, -1.0f},
+            1.0f,
+            (vec2s) {0.0f, 0.0f}
+        };
+        vertices[23] = (Vertex) {
+            (vec3s) {-1.0f, 1.0f, -1.0f},
+            1.0f,
+            (vec2s) {0.0f, 1.0f / 8.0f}
+        };
+        vertices[24] = (Vertex) {
+            (vec3s) {1.0f, -1.0f, 1.0f},
+            1.0f,
+            (vec2s) {1.0f / 8.0f, 0.0f}
+        };
+        vertices[25] = (Vertex) {
+            (vec3s) {1.0f, 1.0f, 1.0f},
+            1.0f,
+            (vec2s) {1.0f / 8.0f, 1.0f / 8.0f}
+        };
+        vertices[26] = (Vertex) {
+            (vec3s) {1.0f, 1.0f, -1.0f},
+            1.0f,
+            (vec2s) {0.0f, 1.0f / 8.0f}
+        };
+        vertices[27] = (Vertex) {
+            (vec3s) {1.0f, 1.0f, -1.0f},
+            1.0f,
+            (vec2s) {0.0f, 1.0f / 8.0f}
+        };
+        vertices[28] = (Vertex) {
+            (vec3s) {1.0f, -1.0f, -1.0f},
+            1.0f,
+            (vec2s) {0.0f, 0.0f}
+        };
+        vertices[29] = (Vertex) {
+            (vec3s) {1.0f, -1.0f, 1.0f},
+            1.0f,
+            (vec2s) {1.0f / 8.0f, 0.0f}
+        };
+        vertices[30] = (Vertex) {
+            (vec3s) {-1.0f, -1.0f, 1.0f},
+            1.0f,
+            (vec2s) {0.0f, 0.0f}
+        };
+        vertices[31] = (Vertex) {
+            (vec3s) {1.0f, -1.0f, 1.0f},
+            1.0f,
+            (vec2s) {1.0f / 8.0f, 0.0f}
+        };
+        vertices[32] = (Vertex) {
+            (vec3s) {1.0f, -1.0f, -1.0f},
+            1.0f,
+            (vec2s) {1.0f / 8.0f, 1.0f / 8.0f}
+        };
+        vertices[33] = (Vertex) {
+            (vec3s) {1.0f, -1.0f, -1.0f},
+            1.0f,
+            (vec2s) {1.0f / 8.0f, 1.0f / 8.0f}
+        };
+        vertices[34] = (Vertex) {
+            (vec3s) {-1.0f, -1.0f, -1.0f},
+            1.0f,
+            (vec2s) {0.0f, 1.0f / 8.0f}
+        };
+        vertices[35] = (Vertex) {
+            (vec3s) {-1.0f, -1.0f, 1.0f},
+            1.0f,
+            (vec2s) {0.0f, 0.0f}
         };
 
-        draw_triangles(4, vertices, &texture, proj, view, model);
+        draw_triangles(12, vertices, &texture, proj, view, model);
 
         long end = ns_now();
         
